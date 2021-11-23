dbbc0ed905f08c41b7706f15687e761a4f8b55f3
Maksym <eldiablo@ukr.net>
2021-11-23 18:32:27 +0200

    animations added

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,13 +9,15 @@ public class Enemy : MonoBehaviour
     public int maxHP;
     public int goldToGive;
     public Image healthBarFill;
+    public Animation anim;
 
     public void Damage()
     {
         currentHP--;
 
         healthBarFill.fillAmount = (float)currentHP / (float)maxHP;
-
+        anim.Stop();
+        anim.Play();
         if(currentHP <= 0)
         {
             Defeated();
